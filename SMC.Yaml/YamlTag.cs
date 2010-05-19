b06240ae01df240cbf69c57af37a407a4c15ba9f
Stephen Czetty <stephen.czetty@bancvue.com>
2010-05-19 14:07:40 -0500

    Some notes, adding specification for Yaml 1.2.

--- SMC.Yaml/YamlTag.cs
+++ SMC.Yaml/YamlTag.cs
@@ -2,6 +2,14 @@ namespace SMC.Yaml
 {
     public class YamlTag
     {
-        
+        public YamlTag(string handle, string prefix)
+        {
+            Handle = handle;
+            Prefix = prefix;
+        }
+
+        public string Handle { get; private set; }
+
+        public string Prefix { get; private set; }
     }
 }

e2b77dc3790b29cc545998b3bee913129cd8a2bc
Stephen Czetty <steve.czetty@gmail.com>
2010-05-26 09:14:28 -0500

    Minor corrections to the grammar.
    Started implementing the C# grammar, using Irony.  Still figuring it out,
    but I think once the BNF grammer is nailed down, it shouldn't be so bad.

--- src/SMC.Yaml/Parser/YamlGrammar.cs
+++ src/SMC.Yaml/Parser/YamlGrammar.cs
@@ -14,10 +14,14 @@ namespace SMC.Yaml.Parser
             var stream = new NonTerminal("Stream");
 
             // Document
+            var documentList = new NonTerminal("DocumentList");
             var document = new NonTerminal("Document");
 
             // Directives
+            var directiveList = new NonTerminal("DirectiveList");
+            var directive = new NonTerminal("Directive");
             var yamlDirective = new NonTerminal("YamlDirective");
+            var tagDirectiveList = new NonTerminal("TagDirectiveList");
             var tagDirective = new NonTerminal("TagDirective");
 
             
@@ -26,10 +30,10 @@ namespace SMC.Yaml.Parser
             Root = stream;
 
             // Stream
-            
+            stream.Rule = MakeStarRule(documentList, document);
 
             // Documents
-            
+            document.Rule = MakeStarRule(directiveList, directive);
         }
     }
 }
